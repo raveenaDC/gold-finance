1b6acea04d9fe3a07ac498bfcbafb4267e2278d2
raveenaRaj <114391801+raveenaDC@users.noreply.github.com>
2025-07-29 23:06:58 +0530

    Update api.config.js

--- gold-loan/src/config/api.config.js
+++ gold-loan/src/config/api.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:4000';
+const API_BASE_URL = 'https://gold-finance.onrender.com';
 
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL
